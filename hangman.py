5f7050b81346ce9591b2a4366038021c9c4ed29f
oviozz <42685801+oviozz@users.noreply.github.com>
2022-02-24 12:39:29 -0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -10,9 +10,13 @@ print(word_picker)
 list_container = ['_' for i in word_picker] # makes container based on the length of the word
 tries = 10
 
-while tries != 0 and ''.join(list_container) != word_picker:
+while  ''.join(list_container) != word_picker:
     user = input(f'Guess a letter{[tries]}: ')
     index_words_finding = [i for i in range(len(word_picker)) if word_picker[i] == user] # makes a list of index in instances if the users input letter is in the word
+
+    tries -= 1
+    if tries == 0:
+        break
 
     if user in word_picker: # Merge the list_container correct index with the value
         for i in index_words_finding:
@@ -21,7 +25,6 @@ while tries != 0 and ''.join(list_container) != word_picker:
         print('Sorry, Wrong Guess!')
         continue
     print(''.join(list_container))
-    tries -= 1
 print()
 
 if ''.join(list_container) == word_picker:
