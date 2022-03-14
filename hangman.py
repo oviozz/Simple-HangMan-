faf5c8ccc4e53214eb06ecb26bb768ee26af034b
oviozz <42685801+oviozz@users.noreply.github.com>
2022-03-14 15:30:26 -0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,24 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+
+word_url = 'https://randomword.com'
+
+request = requests.get(word_url).text
+soup = BeautifulSoup(request,'lxml')
+game_word = soup.find('div',id='random_word').text
 
 
-import random
+list_container = ['_' for i in game_word] # makes container based on the length of the word
+tries = len(game_word) + 5
 
-# Word list
-word = ['apple','banana','cat','dog']
-word_picker = random.choice(word) # random  word picks
-print(word_picker)
-
-list_container = ['_' for i in word_picker] # makes container based on the length of the word
-tries = 10
-
-while  ''.join(list_container) != word_picker:
+while  ''.join(list_container) != game_word:
     user = input(f'Guess a letter{[tries]}: ')
-    index_words_finding = [i for i in range(len(word_picker)) if word_picker[i] == user] # makes a list of index in instances if the users input letter is in the word
+    index_words_finding = [i for i in range(len(game_word)) if game_word[i] == user] # makes a list of index in instances if the users input letter is in the word
 
     tries -= 1
     if tries == 0:
         break
 
-    if user in word_picker: # Merge the list_container correct index with the value
+    if user in game_word: # Merge the list_container correct index with the value
         for i in index_words_finding:
             list_container[i] = user
     else:
@@ -27,10 +29,11 @@ while  ''.join(list_container) != word_picker:
     print(''.join(list_container))
 print()
 
-if ''.join(list_container) == word_picker:
+if ''.join(list_container) == game_word:
     print('Congratz You won!')
 else:
     print('Sorry You Lost, Try again?')
+
 
 
 
